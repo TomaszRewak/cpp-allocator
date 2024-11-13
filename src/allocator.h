2d81518ff19d0e4eddaaa09f8103f08356719b00
TomaszRewak <tomaszrewak@gmail.com>
2024-11-14 01:46:48 +0800

    Workin on the allocator

--- src/allocator.h
+++ src/allocator.h
@@ -1,9 +1,37 @@
 #pragma once
 
+#include <span>
+
 namespace allocator {
 
-int add(int a, int b) {
-    return a + b;
-}
+template <std::size_t Size>
+class memory final {
+private:
+    using pointer_type = uint64_t; // TODO: determine it based on the size of the memory
+    using size_type = uint64_t; // TODO: determine it based on the size of the memory
+
+    struct allocation_header final {
+        pointer_type previous;
+        pointer_type next;
+        pointer_type previous_free;
+        pointer_type next_free;
+    };
+
+public:
+    std::span<std::byte> allocate(size_t size) {
+        const auto data = std::span<std::byte>(_data);
+        return data.subspan(0, size);
+    }
+
+    template <typename T, typename... Args>
+    T* allocate(Args&&... args) {
+        const auto allocated = allocate(sizeof(T));
+        return new (allocated.data()) T(std::forward<Args>(args)...);
+    }
+
+private:
+    std::array<allocation*, 2> _nodes;
+    std::byte _data[Size];
+};
 
 }
--- tests/test_allocator.cc
+++ tests/test_allocator.cc
@@ -3,8 +3,20 @@
 
 namespace allocator {
 
-TEST(AllocatorTest, Add) {
-    EXPECT_EQ(3, add(1, 2));
+TEST(AllocatorTest, SimpleAllocation) {
+    memory<1024> memory;
+    const auto* value = memory.allocate<int>(42);
+
+    ASSERT_EQ(*value, 42);
+}
+
+TEST(AllocatorTest, TwoSimpleAllocations) {
+    memory<1024> memory;
+    const auto* value1 = memory.allocate<int>(42);
+    const auto* value2 = memory.allocate<int>(43);
+
+    ASSERT_EQ(*value1, 42);
+    ASSERT_EQ(*value2, 43);
 }
 
 }
